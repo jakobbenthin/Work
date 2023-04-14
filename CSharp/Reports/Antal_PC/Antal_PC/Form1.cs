eb9b3f15f9b87af99076c4d5c16dc3327e3a120b
Jakob Benthin <jakob.benthin@gmail.com>
2023-04-14 14:13:23 +0200

    added for c#

---
+++ CSharp/Reports/Antal_PC/Antal_PC/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Diagnostics;
+using Microsoft.PowerShell;
+using System.IO;
+
+namespace Antal_PC
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string powerShellExe = @"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe";
+            string scriptFile = @"C:\temp\ps.ps1";
+            string param1 = textBox1.Text;
+            string param2 = "value2";
+
+            // Create a new process and configure it to run the PowerShell script with parameters
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = powerShellExe;
+            psi.Arguments = $"-File \"{scriptFile}\" -ivantiFile \"{param1}\" -ivantiSheet \"{param2}\"";
+
+            // Start the process and wait for it to exit
+            Process p = Process.Start(psi);
+            p.WaitForExit();
+        }
+        private void btn_file_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            textBox1.Text = openFileDialog.FileName;
+        }
+    }
+}
---
+++ CSharp/Reports/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management.Automation;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void Test()
+        {
+
+            // create a PowerShell instance
+            PowerShell ps = PowerShell.Create();
+
+            // specify the PowerShell script to run
+            ps.AddScript("path/to/your/script.ps1");
+
+            // add parameters to the script
+            ps.AddParameter("param1", "value1");
+            ps.AddParameter("param2", "value2");
+
+            // execute the script
+            ps.Invoke<Collection<PSObject>>();
+
+        }
+    }
+}
